74981b2b603740d810bc3c867a3deec5c80b12f7
Hermansyah Putra <hermansyahputra0418@gmail.com>
2022-09-24 00:38:39 +0700

    Fix

--- frontend/src/components/SeriesList.jsx
+++ frontend/src/components/SeriesList.jsx
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 
 function SeriesList({ id, seriesImg, title, year }) {
   return (
-    <Link to={`/video/${id}`} className="text-decoration-none">
+    <Link to={`/detail-movies/${id}`} className="text-decoration-none">
       <Card style={{ backgroundColor: "#1f1f1f" }} className="rounded border-0 mt-5 text-white">
         <Card.Img variant="top" src={seriesImg} />
         <Card.Body>
-          <Card.Title>{title}</Card.Title>
+          <Card.Title style={{ fontSize: "18px" }}>{title}</Card.Title>
           <p className="text-muted">{year}</p>
         </Card.Body>
       </Card>
